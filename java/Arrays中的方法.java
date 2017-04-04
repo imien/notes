381ae3593d0f120e1aec16ba118b2cd40ed9ed74
imien <zhangkuisoftware@163.com>
2017-04-04 16:05:38 +0800

    Create Arrays中的方法.java

---
+++ java/Arrays中的方法.java
@@ -0,0 +1,9 @@
+public static <T> List<T> asList(T... a) 将对象数组转化为链表
+
+public static int binarySearch(long[] a, long key) 二分法搜索，返回位置（使用该方法前，必须先调用Arrays.sort()方法）
+
+public static void sort(double[] a) 数组排序，默认升序
+
+public static byte[] copyOf(byte[] original, int newLength) 数组拷贝，newLength的长度大于待拷数组时，多余的位置放默认值
+
+public static boolean equals(short[] a, short a2[]) 比较两个数组是否相等
